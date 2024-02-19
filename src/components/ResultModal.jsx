5082b291248b747ba78ac755ff2e17c68f21b5d0
httpsanuj1807 <anuj1316.be22@chitkara.edu.in>
2024-02-19 15:46:43 +0530

    Score functionality added

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -5,6 +5,7 @@ const ResultModal = forwardRef(
         const dialog = useRef();
         const userLost  = timeRemaining <= 0;
         const formattedTimeRemaining = (timeRemaining / 1000).toFixed(2);
+        const score = Math.round((1 - timeRemaining / (targetTime * 1000)) * 100);
         useImperativeHandle(ref, ()=>{
             return {
                 open (){
@@ -15,7 +16,7 @@ const ResultModal = forwardRef(
         return(
             <dialog ref={dialog} className="result-modal">
                 {userLost && <h2>You Lost!</h2>}
-                {!userLost && <h2>You Won!</h2>}
+                {!userLost && <h2>You Score:{score}</h2>}
                 <p>The target time was <strong>{targetTime} seconds.</strong></p>
                 <p>You stopped the timer with <strong>{formattedTimeRemaining} seconds left.</strong></p>
                 <form method="dialog" onSubmit={handleReset}>
